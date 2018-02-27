644a604b5a70349fba95ff71af98b8f12e5748f1
Tom Dohnal <tomas.dohnal@gmail.com>
2018-02-27 15:23:41 +0100

    Server rendering styled-jsx issue fixed.

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import flush from 'styled-jsx/server';
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const { html, head, errorHtml, chunks } = renderPage();
-    return { html, head, errorHtml, chunks };
+    const styles = flush();
+    return { html, head, errorHtml, chunks, styles };
   }
 
   render() {
